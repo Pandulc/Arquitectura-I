c4a84a2e51400f0d8aece53119d8cc5095680afe
Gabriel <gabiguibe@gmail.com>
2022-11-11 11:08:53 -0300

    README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Proyecto Integrador 
+## Arquitectura de Computadoras I
+
+Proyecto correspondiente a la materia Arquitectura de Computadoras I, basado en la implementacion de un programa en assembly para el manejo de la placa DE0-nano
+
+### Integrantes
+
+![MattGoode7](https://github.com/MattGoode7.png?size=70) [Matias Carrizo](https://github.com/MattGoode7)
+
+![Pandulc](https://github.com/Pandulc.png?size=70) [Gabriel Guillaumet](https://github.com/Pandulc)
--- arm.sv
+++ arm.sv
@@ -33,7 +33,7 @@ module dmem(input  logic        clk, we,
   logic [31:0] RAM[63:0];
   
   initial
-      $readmemh("E:\\Desktop\\Arquitectura-I-main\\dmem.dat",RAM);
+      $readmemh("E:\\Desktop\\Arquitectura-I\\dmem.dat",RAM);
 
   assign rd = RAM[a[31:2]]; // word aligned
 
@@ -47,7 +47,7 @@ module imem(input  logic [31:0] a,
   logic [31:0] RAM[255:0];
 
   initial
-      $readmemh("E:\\Desktop\\Arquitectura-I-main\\imem.dat",RAM);
+      $readmemh("E:\\Desktop\\Arquitectura-I\\imem.dat",RAM);
 
   assign rd = RAM[a[31:2]]; // word aligned
 endmodule
